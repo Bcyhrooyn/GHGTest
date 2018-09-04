bc249b3f914793eca3a1cb60e41681e7318161d8
Byron Choy <Bcyhrooyn@gmail.com>
2018-09-04 12:18:16 -0700

    Added start menu with controller support

---
+++ GHGTest/Assets/Scripts/StartMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour {
+
+    public void OnStartButtonClicked()
+    {
+        SceneManager.LoadScene("MainGame");
+    }
+
+    public void OnQuitButtonClicked()
+    {
+        Application.Quit();
+    }
+}
